bd11dd18a371199a8871be77e0000a1315baa689
Narumi Nogawa <naruo0711@ab.auone-net.jp>
2019-11-09 15:12:00 +0900

    Initial Commit

---
+++ FaceOthello/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FaceOthello
+//
+//  Created by Narumi Nogawa on 2019/11/09.
+//  Copyright © 2019 Narumi Nogawa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
