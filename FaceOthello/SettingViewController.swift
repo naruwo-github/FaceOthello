0a59dae5cf154d2b46bd3da77623b38886829269
Narumi Nogawa <naruo0711@ab.auone-net.jp>
2019-12-05 22:01:39 +0900

    userdefaultsの拡張

--- FaceOthello/SettingViewController.swift
+++ FaceOthello/SettingViewController.swift
@@ -15,9 +15,13 @@ class SettingViewController: UIViewController, UIImagePickerControllerDelegate, 
     @IBOutlet weak var browseButton: UIButton!
     @IBOutlet weak var playButton: UIButton!
     
+    let userDefault = UserDefaults.standard
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        profileImageView.contentMode = .scaleAspectFill
+        
     }
     
     @IBAction func selectPhotoButtonTapped(_ sender: Any) {
@@ -38,6 +42,27 @@ class SettingViewController: UIViewController, UIImagePickerControllerDelegate, 
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
         let image = info[UIImagePickerController.InfoKey.originalImage] as! UIImage
         self.profileImageView.image = image
+        
         self.dismiss(animated: true, completion: nil)
     }
+}
+
+extension UserDefaults {
+    // 保存したいUIImage, 保存するUserDefaults, Keyを取得
+    func setUIImageToData(image: UIImage, forKey: String) {
+        // UIImageをData型へ変換
+        let nsdata = image.pngData()
+        // UserDefaultsへ保存
+        self.set(nsdata, forKey: forKey)
+    }
+    // 参照するUserDefaults, Keyを取得, UIImageを返す
+    func image(forKey: String) -> UIImage {
+        // UserDefaultsからKeyを基にData型を参照
+        let data = self.data(forKey: forKey)
+        // UIImage型へ変換
+        let returnImage = UIImage(data: data!)
+        // UIImageを返す
+        return returnImage!
+    }
+
 }
