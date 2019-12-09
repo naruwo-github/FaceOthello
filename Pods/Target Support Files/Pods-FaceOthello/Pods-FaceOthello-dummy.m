0ae0b9748c6a4d24dda009fc1dcc715b660a0f68
Narumi Nogawa <naruo0711@ab.auone-net.jp>
2019-12-09 16:45:56 +0900

    cropviewcontroller

---
+++ Pods/Target Support Files/Pods-FaceOthello/Pods-FaceOthello-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_FaceOthello : NSObject
+@end
+@implementation PodsDummy_Pods_FaceOthello
+@end
---
+++ Pods/Target Support Files/Pods-FaceOthelloTests/Pods-FaceOthelloTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_FaceOthelloTests : NSObject
+@end
+@implementation PodsDummy_Pods_FaceOthelloTests
+@end
