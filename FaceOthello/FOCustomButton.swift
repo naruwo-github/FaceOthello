12d241255be704d5c58ea82268ecae82df29d8a7
Narumi Nogawa <naruo0711@ab.auone-net.jp>
2020-09-29 00:22:33 +0900

    IBInspectableの設定（UIButton）

--- FaceOthello/FOCustomButton.swift
+++ FaceOthello/FOCustomButton.swift
@@ -6,4 +6,73 @@
 //  Copyright © 2020 Narumi Nogawa. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+@IBDesignable
+class FOCustomButton: UIButton {
+    @IBInspectable var borderColor: UIColor = UIColor.clear {
+        didSet {
+            layer.borderColor = self.borderColor.cgColor
+        }
+    }
+    
+    @IBInspectable var borderWidth: CGFloat = 0 {
+        didSet {
+            layer.borderWidth = self.borderWidth
+        }
+    }
+    
+    @IBInspectable var cornerRadius: CGFloat = 0 {
+        didSet {
+            layer.cornerRadius = self.cornerRadius
+        }
+    }
+    
+    @IBInspectable var shadowOffsetWidth: CGFloat = 2.0 {
+        didSet {
+            layer.shadowOffset.width = self.shadowOffsetWidth
+        }
+    }
+    
+    @IBInspectable var shadowOffsetHeight: CGFloat = 2.0 {
+        didSet {
+            layer.shadowOffset.height = self.shadowOffsetHeight
+        }
+    }
+    
+    @IBInspectable var shadowColor: UIColor = UIColor.black {
+        didSet {
+            layer.shadowColor = self.borderColor.cgColor
+        }
+    }
+    
+    @IBInspectable var shadowRadius: CGFloat = 4.0 {
+        didSet {
+            layer.shadowRadius = self.shadowRadius
+        }
+    }
+    
+    @IBInspectable var shadowOpacity: Float = 0.4 {
+        didSet {
+            layer.shadowOpacity = self.shadowOpacity
+        }
+    }
+    
+    @IBInspectable var enableShadow: Bool = false {
+        didSet {
+            if self.enableShadow {
+                self.setButtonShadow()
+            }
+        }
+    }
+    
+    private func setButtonShadow() {
+        self.clipsToBounds = false
+        let layer = self.layer
+        layer.masksToBounds = false
+        layer.shadowOffset = CGSize(width: self.shadowOffsetWidth, height: self.shadowOffsetHeight)
+        layer.shadowColor = self.shadowColor.cgColor
+        layer.shadowRadius = self.shadowRadius
+        layer.shadowOpacity = self.shadowOpacity
+    }
+}
