9d95d5463471e6493aea19789b658c0ac21e1cc4
Narumi Nogawa <naruo0711@ab.auone-net.jp>
2020-09-30 23:32:15 +0900

    端末で通信の許可とsoketio記述

--- FaceOthello/FORoomCreateEnterViewController.swift
+++ FaceOthello/FORoomCreateEnterViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import SocketIO
 
 class FORoomCreateEnterViewController: UIViewController {
     @IBOutlet private weak var profileImageView: UIImageView!
@@ -17,6 +18,8 @@ class FORoomCreateEnterViewController: UIViewController {
         super.viewDidLoad()
         
         self.setProfileImage()
+        self.setupTextField()
+        self.setupSocketIO()
     }
 
     func setup(profileImage: UIImage) {
@@ -29,6 +32,33 @@ class FORoomCreateEnterViewController: UIViewController {
         }
     }
     
+    private func setupTextField() {
+        // キーボードは数字のみのものを扱う
+        self.roomIdTextField.keyboardType = UIKeyboardType.numberPad
+        self.roomIdTextField.attributedPlaceholder = NSAttributedString(string: "Room ID", attributes: [NSAttributedString.Key.foregroundColor : UIColor.lightGray])
+    }
+    
+    private func setupSocketIO() {
+        let manager = SocketManager(socketURL: URL(string: "http://localhost:3000")!, config: [.log(true), .compress])
+        let socket = manager.defaultSocket
+        
+        socket.on(clientEvent: .connect) {data, ack in
+            print("socket connected")
+        }
+
+        socket.on("currentAmount") {data, ack in
+            guard let cur = data[0] as? Double else { return }
+            
+            socket.emitWithAck("canUpdate", cur).timingOut(after: 0) {data in
+                socket.emit("update", ["amount": cur + 2.50])
+            }
+
+            ack.with("Got your currentAmount", "dude")
+        }
+
+        socket.connect()
+    }
+    
     @IBAction private func createRoomButtonTapped(_ sender: Any) {
         // TODO: サーバー側にルームID取得をリクエスト
         // ルームIDを受け取ったらマッチング画面へ遷移する
@@ -37,5 +67,15 @@ class FORoomCreateEnterViewController: UIViewController {
     @IBAction private func enterRoomButtonTapped(_ sender: Any) {
         // TODO: RoomIdが入力済みならそのIdのルームが存在するかサーバー側にリクエスト
         // 存在する場合はルームに入る＝マッチング画面へ遷移
+        if let roomId = self.roomIdTextField.text, !roomId.isEmpty {
+            // ルームidリクエスト処理
+        }
+    }
+    
+    @IBAction func beginEditTextfield(_ sender: Any) {
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.roomIdTextField.resignFirstResponder()
     }
 }
