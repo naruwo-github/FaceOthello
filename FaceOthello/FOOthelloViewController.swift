c97921045ef519eda66e887490e44dfe997fc402
Narumi Nogawa <naruo0711@ab.auone-net.jp>
2020-10-22 16:00:45 +0900

    セルフつけたかった

--- FaceOthello/FOOthelloViewController.swift
+++ FaceOthello/FOOthelloViewController.swift
@@ -41,7 +41,7 @@ class FOOthelloViewController: UIViewController {
     
     private func createUI(w: CGFloat, h: CGFloat) {
         self.board.start(size: self.BOARDSIZE, isFirstStrike: true)
-        let stoneSideLength = w / CGFloat(BOARDSIZE + 1)
+        let stoneSideLength = w / CGFloat(self.BOARDSIZE + 1)
         let stoneStartX = w / 2 - (stoneSideLength + 1) * 4
         let stoneStartY = h / 2 - (stoneSideLength + 1) * 4
         
@@ -69,9 +69,9 @@ class FOOthelloViewController: UIViewController {
 
     @objc func pushed(mybtn: ButtonClass) {
         mybtn.isEnabled = false
-        board.put(x: mybtn.x, y: mybtn.y, stone: USER_COLOR)
+        self.board.put(x: mybtn.x, y: mybtn.y, stone: self.USER_COLOR)
         drawBoard()
-        if board.isGameOver() == true {
+        if self.board.isGameOver() == true {
             self.switchButtonAppearance(button: self.retryButton, isEnabled: true)
         }
         
@@ -81,19 +81,19 @@ class FOOthelloViewController: UIViewController {
     }
 
     private func CpuTurn() {
-        if board.available(stone: CPU_COLOR).count != 0 {
-            let xy = player.play(board: board, stone: CPU_COLOR)
-            board.put(x: xy.0, y: xy.1, stone: CPU_COLOR)
-            drawBoard()
-            if board.isGameOver() == true {
+        if self.board.available(stone: self.CPU_COLOR).count != 0 {
+            let xy = player.play(board: self.board, stone: self.CPU_COLOR)
+            self.board.put(x: xy.0, y: xy.1, stone: self.CPU_COLOR)
+            self.drawBoard()
+            if self.board.isGameOver() == true {
                 self.switchButtonAppearance(button: self.retryButton, isEnabled: true)
             }
         }
-        if board.isGameOver() == true {
+        if self.board.isGameOver() == true {
             self.switchButtonAppearance(button: self.retryButton, isEnabled: true)
             self.navigationItem.hidesBackButton = false
         }
-        if board.available(stone: USER_COLOR).count == 0 {
+        if self.board.available(stone: self.USER_COLOR).count == 0 {
             self.switchButtonAppearance(button: self.passButton, isEnabled: true)
         }
     }
@@ -104,25 +104,25 @@ class FOOthelloViewController: UIViewController {
         self.cpuStoneCountLabel.text = "CPU: \(stonecount.1)"
         
         var count = 0
-        let _board = board.returnBoardState()
-        for y in 0..<BOARDSIZE {
-            for x in 0..<BOARDSIZE {
-                if _board[y][x] == USER_COLOR {
-                    buttonArray[count].setImage(myStoneImage, for: .normal)
-                } else if _board[y][x] == CPU_COLOR {
-                    buttonArray[count].setImage(cpuStoneImage, for: .normal)
+        let _board = self.board.returnBoardState()
+        for y in 0..<self.BOARDSIZE {
+            for x in 0..<self.BOARDSIZE {
+                if _board[y][x] == self.USER_COLOR {
+                    self.buttonArray[count].setImage(self.myStoneImage, for: .normal)
+                } else if _board[y][x] == self.CPU_COLOR {
+                    self.buttonArray[count].setImage(self.cpuStoneImage, for: .normal)
                 } else {
-                    buttonArray[count].setImage(baseBoardImage, for: .normal)
+                    self.buttonArray[count].setImage(self.baseBoardImage, for: .normal)
                 }
-                buttonArray[count].isEnabled = false
+                self.buttonArray[count].isEnabled = false
                 count += 1
             }
         }
-        let availableList = board.available(stone: USER_COLOR)
+        let availableList = self.board.available(stone: self.USER_COLOR)
         for i in 0..<(availableList.count) {
             let x = availableList[i][0]
             let y = availableList[i][1]
-            buttonArray[x*BOARDSIZE+y].isEnabled = true
+            self.buttonArray[x * self.BOARDSIZE + y].isEnabled = true
         }
     }
     
